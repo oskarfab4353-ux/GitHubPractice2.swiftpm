2944d695bba95d499bb1c39cf84fd2e66cbb5456
OskarF <oskarfab4353@gmail.com>
2026-04-15 10:39:03 -0500

    i added a textfield with the number 1 variable added

--- ContentView.swift
+++ ContentView.swift
@@ -4,11 +4,6 @@ struct ContentView: View {
     @State var number1: Int = 0
     @State var number2: Int
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
+        TextField("Enter first number", value: $number1, format: .number)
     }
 }
