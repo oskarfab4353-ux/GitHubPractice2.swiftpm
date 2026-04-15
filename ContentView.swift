d15727c0affc1de96204e704637a6dc732d1168d
Roberto <rgomez9148@stu.d214.org>
2026-04-15 10:42:05 -0500

    added textfield2

--- ContentView.swift
+++ ContentView.swift
@@ -4,11 +4,6 @@ struct ContentView: View {
     @State var number1: Int = 0
     @State var number2: Int
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
+        TextField("enter another number", value: $number2, format: .number)
     }
 }
